dab02df93256e3ec91ec444d23b9f527f31a3cbe
NickArch94 <158612778+NickArch94@users.noreply.github.com>
2025-07-04 13:55:29 -0600

    Added some more styling to my component-holder boxes, and added some animation to the 'Struggles & Lessons' cards.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,44 @@
 import 'flowbite'
+import {gsap} from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger);
 
 
+gsap.fromTo('.cards', {
+    opacity: 0,
+    scale: 1,
+    rotation: 360,
+    x: "-500%"
+}, {
+    scrollTrigger: {
+        trigger: '.main-section',
+        start: 'top center',
+        markers: true,
+        toggleActions: "play none none reverse"
+    },
+    duration: 2,
+    opacity: 1,
+    scale: 1,
+    rotation: 0,
+    x: 0,
+    ease: "power2.inOut"
+    })
+
+const donationButton = document.getElementById('donateButton')
+const donationDropdown = document.getElementById('donateDropdown')
+
+donationButton.addEventListener('click', (e) => {
+    e.stopPropagation()
+    donationDropdown.classList.toggle('hidden')
+})
+
+document.addEventListener('click', (e) => {
+    if (!donationButton.contains(e.target)) {
+        donationDropdown.classList.add('hidden')
+    }
+})
+
+donationDropdown.addEventListener('click', (e) => {
+    e.stopPropagation()
+})

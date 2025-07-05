30c9719f91c1dac08b0fbbd896c069fac4a72953
NickArch94 <158612778+NickArch94@users.noreply.github.com>
2025-07-05 09:25:22 -0600

    Added more animations to the 'Support & Motivation' section.

--- src/main.js
+++ src/main.js
@@ -25,6 +25,42 @@ gsap.fromTo('.cards', {
     ease: "power2.inOut"
     })
 
+gsap.fromTo(".text-journey", {
+    opacity: 0,
+    y: "-500%"
+}, {
+    duration: 1.0,
+    opacity: 1,
+    y: 0,
+    stagger: 0.5,
+    ease: "power2.inOut"
+    })
+
+gsap.from(".journey-paragraph-1", {
+    duration: 1.0,
+    opacity: 0,
+    ease: "power2.inOut"
+})
+
+gsap.from(".text-journey-02", {
+    duration: 1.0,
+    opacity: 0,
+    stagger: 0.5,
+    ease: "power2.in"
+})
+
+gsap.from(".text-support", {
+    duration: 2.0,
+    opacity: 0,
+    ease: "power2.inOut",
+    ScrollTrigger: {
+        trigger: ".Motivation-Section",
+        start: "top center",
+        markers: true,
+        toggleActions: "play none none reverse"
+    }
+})
+
 const donationButton = document.getElementById('donateButton')
 const donationDropdown = document.getElementById('donateDropdown')
 
